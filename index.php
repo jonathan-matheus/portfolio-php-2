ac196a9b91673362ac1db41805a03e636f7d624d
jonathan-matheus <johhn.dev@gmail.com>
2025-10-01 11:59:51 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/output.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="bg-[url('/img/bg-intro.png')] bg-cover bg-no-repeat pt-60">
+        <div class="container mx-auto">
+            <div class="flex justify-center">
+                <img src="./img/avatar.png" alt="avatar">
+            </div>
+            <div class="flex flex-col justify-center text-center pt-10">
+                <p class="text-gray-200 text-base">
+                    Hello World! Meu nome é <span class="text-red-900">
+                        Martina Santos
+                    </span>
+                    e sou
+                </p>
+                <h1 class="text-5xl text-gray-100 pt-2">Desenvolvedora PHP</h1>
+                <p class="text-gray-300 text-sm pt-4 mx-120">
+                    Transformo necessidades em aplicações reais, evolventes e
+                    funcionais. Desenvolvo sistemas através da
+                    minha paixão pela tecnologia, contribuindo com soluções
+                    inovadoras e eficazes para desafios
+                    complexos.
+                </p>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
